82901370950ca0c35fc14224a94c9c8f71320d0e
Priyanka Sawant <priyankasawant11888@gmail.com>
2024-05-02 13:24:43 +0530

    initial commit

---
+++ app/src/test/java/qtriptest/tests/DriverSingleton.java
@@ -0,0 +1,11 @@
+// package qtriptest.tests;
+
+// import org.openqa.selenium.remote.RemoteWebDriver;
+
+// public class DriverSingleton {
+
+//     public static RemoteWebDriver getDriverInstance(String string) {
+//         return null;
+//     }
+
+// }
